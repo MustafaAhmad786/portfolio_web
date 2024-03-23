903a8d9f9c986457a8fea75d579500b8aa1704fb
DESKTOP-86LEAEA\Abdullah <abdullahktk760@gmail.com>
2024-03-23 12:49:09 +0500

    Recommention changes with container

--- lib/Home/home_screen.dart
+++ lib/Home/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:portofilo/Screen_/allcontainerproject/my_project.dart';
+import 'package:portofilo/Models/recommendation.dart';
+import 'package:portofilo/Screen_/all_container_project/my_project.dart';
 import 'package:portofilo/Screen_/main_screen.dart';
 import 'package:portofilo/component/home_banner.dart';
+import 'package:portofilo/constraints.dart';
 import 'package:portofilo/newbar_code.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -19,6 +21,39 @@ class HomeScreen extends StatelessWidget {
             /// just like 10M+ subscibe, 750+ video ,8+ Github Project , 100+ Star
             ),
         MyProject(),
+        Column(
+          children: [
+            Text('Recommention',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge // es ka jak use huta hain headline6 old,
+                ),
+            Container(
+              width: 400,
+              padding: EdgeInsets.all(defaultpadding),
+              color: secondarycolor,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    demo_recommendations[0].name!,
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    demo_recommendations[0].source!,
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    demo_recommendations[0].text!,
+                    style: Theme.of(context).textTheme.titleSmall,
+                  )
+                ],
+              ),
+            )
+          ],
+        )
       ],
     );
   }
